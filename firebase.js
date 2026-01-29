cc5e5706ac174216b41650c719ab632b54ffd80d
mobjle2 <lktrungnguyen@gmail.com>
2026-01-29 11:08:08 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
+// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔥 DÁN CẤU HÌNH FIREBASE CỦA BẠN VÀO ĐÂY
+// 🔥 CẤU HÌNH FIREBASE (ĐÃ ĐIỀN SẴN)
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
+  apiKey: "AIzaSyBjBZJuSzt4JQDXvDyAXztTgo75qv6u5R8",
   authDomain: "mobjle21982.firebaseapp.com",
   projectId: "mobjle21982",
-  appId: "PASTE_YOUR_APP_ID"
+  appId: "1:1034646220657:web:7722953f721845b8e5d0d0"
 };
 
 // Khởi tạo Firebase
 export const app = initializeApp(firebaseConfig);
 
-// Firebase Auth & Firestore
+// Firebase Authentication & Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
